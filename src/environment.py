5af05e34be59ad160ee4b7fb582857330e563c02
Alessio Borgi <alessioborgi3@gmail.com>
2025-07-15 14:47:40 +0900

    Update filtered_image salt and pepper

--- src/environment.py
+++ src/environment.py
@@ -9,7 +9,7 @@ if img is None:
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 2) Threshold to segment rocks.
-threshold_value = 130   
+threshold_value = 120   
 _, thresh = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY_INV)
 
 # 3) Detect contours (rocks).
@@ -26,7 +26,7 @@ for x in range(0, occupancy_grid.shape[1], cell_size):
     cv2.line(grid_img, (x, 0), (x, occupancy_grid.shape[0]), (0, 255, 0), 1)
 for y in range(0, occupancy_grid.shape[0], cell_size):
     cv2.line(grid_img, (0, y), (occupancy_grid.shape[1], y), (0, 255, 0), 1)
-cv2.imwrite('/Users/alessioborgi/Documents/GitHub/Tohoku_TESP/img/occupancy_grid_with_gridlines.png', grid_img)
+cv2.imwrite('/Users/alessioborgi/Documents/GitHub/Tohoku_TESP/img/occupancy_grid_with_gridlines1.png', grid_img)
 
 # 6) Export the occupancy grid as PNG.
-cv2.imwrite('/Users/alessioborgi/Documents/GitHub/Tohoku_TESP/img/occupancy_grid.png', occupancy_grid)
+cv2.imwrite('/Users/alessioborgi/Documents/GitHub/Tohoku_TESP/img/occupancy_grid1.png', occupancy_grid)
