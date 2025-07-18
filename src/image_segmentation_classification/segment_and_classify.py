a92eb205da46db8918ba7d004d39d5dae6612754
Alessio Borgi <alessioborgi3@gmail.com>
2025-07-18 15:14:28 +0900

    First try YoLo training

--- src/image_segmentation_classification/segment_and_classify.py
+++ src/image_segmentation_classification/segment_and_classify.py
@@ -8,4 +8,6 @@ rf = Roboflow(api_key="hh1Q7q5T4PDmmGAIEqrB")
 project = rf.workspace("tespproject2025").project("manualsegmentation")
 version = project.version(4)
 dataset = version.download("darknet")
+
+
                 
